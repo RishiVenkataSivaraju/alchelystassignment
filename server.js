99b35924b139c2c42b0d8c84477b9de6e439d84e
Rishi Sivaraju <sivarajurishi@gmail.com>
2025-06-05 21:11:19 +0530

    Deployment Vercel Added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { scheduleJob, listJobs } = require('./scheduler'); // adjust path if needed
-
+const path= require("path");
 const app = express();
 app.use(express.json());
 
